b2f761a93453da85944c6442af868352ec0eb411
Eric Torunski <etorunski@gmail.com>
2022-09-19 11:34:06 -0400

    Finished text widgets

--- app/src/main/java/com/example/torunse/MainActivity.java
+++ app/src/main/java/com/example/torunse/MainActivity.java
@@ -3,14 +3,31 @@ package com.example.torunse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.torunse.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding variableBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //loads the XML file on Screen:
-        setContentView(R.layout.activity_main);
+        variableBinding = ActivityMainBinding.inflate( getLayoutInflater() ); //will load my premade variables from ViewBinding
+
+
+//only 1 function can use Lambda functions
+        variableBinding.firstButton.setOnClickListener(   ( v ) -> {
+            variableBinding.firstString.setText("You clicked on the button");
+            variableBinding.secondString.setText("You clicked on the button");
+        }  ); //onClick function based on looking up documentation
+
+
+        setContentView( variableBinding.getRoot()); //this shows the objects on screen
     }
 }

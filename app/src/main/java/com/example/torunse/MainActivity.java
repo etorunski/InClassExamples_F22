c3c3fd5272fb54eb0aa33ae05ce87425ab0279cb
Eric Torunski <etorunski@gmail.com>
2022-10-03 11:49:18 -0400

    finihsed saving files

--- app/src/main/java/com/example/torunse/MainActivity.java
+++ app/src/main/java/com/example/torunse/MainActivity.java
@@ -7,14 +7,23 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.torunse.databinding.ActivityMainBinding;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    Bitmap mBitmap;
 
     ActivityResultCallback callback = new ActivityResultCallback<ActivityResult>() {
         @Override
@@ -22,8 +31,36 @@ public class MainActivity extends AppCompatActivity {
             if (result.getResultCode() == Activity.RESULT_OK) {
 
                 Intent data = result.getData();
-                String login = data.getStringExtra("LoginName");
-                int amount = data.getIntExtra("Amount", 0);
+
+
+
+                FileOutputStream fOut = null;
+
+
+                try { fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+
+
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+
+
+                    fOut.flush();
+
+
+                    fOut.close();
+
+
+                }
+
+
+                catch (FileNotFoundException e)
+
+
+                { e.printStackTrace();
+
+
+                }
+
+
                 Log.w("MainActivity", "Received " + login + " amount:"+amount);
             }
         }
@@ -39,19 +76,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        File myImage = new File("Picture.png");
+        if(myImage.exists()){ //load your profile picture
+            Bitmap theImage = BitmapFactory.decodeFile("Picture.png");
+            myImageView.setImageBitmap( theImage);
+        }
+
         Log.e("MainActivity", "In onCreate()");
         //loads the XML file on Screen:
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+       String initialLogin= prefs.getString("LoginName", "");//should be tttttttt from MyData.xml
+
+
+binding.loginText.setText(initialLogin);//put the previous version on screen
+
+
 
         binding.button.setOnClickListener(click ->
                 //do this when you click the button:
         {
+            String userTyped = binding.loginText.getText().toString();
+//save this for next time:
+            SharedPreferences.Editor editor = prefs.edit(); //let you save info to disk
+            editor.putString("LoginName", userTyped);
+            editor.commit();//save the file
 
+//data/data/packagename/shared_prefs
 
             Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
-             nextPage.putExtra("LoginName", binding.loginText.getText().toString());//save what was typed under name "LoginName"
+             nextPage.putExtra("LoginName", userTyped);//save what was typed under name "LoginName"
             nextPage.putExtra("Age", 25);
 
 

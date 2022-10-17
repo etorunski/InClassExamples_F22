16a0c4dee7d953aff322b459385710a76cac75d8
Eric Torunski <etorunski@gmail.com>
2022-10-17 10:55:18 -0400

    finished UnitTests

---
+++ app/src/androidTest/java/com/example/torunse/MainActivityTest2.java
@@ -0,0 +1,85 @@
+package com.example.torunse;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest2 {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest2() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+        appCompatEditText.perform(replaceText("ABC123"));
+
+        appCompatEditText.perform(closeSoftKeyboard());
+
+        appCompatEditText.check( matches( withText("ABC123")  ) );
+    }
+
+
+    @Test
+    public void mainActivityTest3() {
+
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+        appCompatEditText.perform(replaceText("ABC123"));
+
+        appCompatEditText.perform(closeSoftKeyboard());
+
+        appCompatEditText.check( matches( withText("ABC123")  ) );
+
+
+        ViewInteraction button = onView(withId(R.id.button2));
+        button.perform(click()); //simulates clicking the button
+
+        ViewInteraction theTextView = onView(withId(R.id.theText));
+        theTextView.check( matches (withText("You clicked on the button")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
--- app/src/main/java/com/example/torunse/MainActivity.java
+++ app/src/main/java/com/example/torunse/MainActivity.java
@@ -3,8 +3,23 @@ package com.example.torunse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+
+/** @author Eric Torunski
+ *  @version 1.0
+ *  @since October 17, 2022
+ */
 public class MainActivity extends AppCompatActivity {
+
+    /** This holds the login button reference   */
+    protected Button lButton;
+
+    /** This holds the editText reference for login*/
+
+    protected EditText loginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +27,25 @@ public class MainActivity extends AppCompatActivity {
 
         //loads the XML file on Screen:
         setContentView(R.layout.activity_main);
+        lButton = findViewById(R.id.button2);
+         loginText = findViewById(R.id.editText);
+
+         TextView tv = findViewById(R.id.theText);
+
+         lButton.setOnClickListener( click ->
+                 tv.setText("You clicked the button")
+                 );
+
+        stringHas123(loginText.getText().toString());
+    }
+
+
+    /** This function checks if s has the string "123" somewhere in it
+     *
+     * @param s Is the string that we are checking for "123"
+     * @return Returns true if s has "123", false otherwise
+     */
+    public boolean stringHas123(String s){
+        return s.contains("123");
     }
 }

29dd1d5c42a2e80702bbe8bc72f9a34ca2e63e1c
etoru <cxbfxzbfsd>
2022-11-28 10:44:02 -0500

    showed how to auth headers

--- app/src/main/java/com/example/torunse/MainActivity.java
+++ app/src/main/java/com/example/torunse/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -21,11 +22,14 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.torunse.databinding.ActivityMainBinding;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /** @author Eric Torunski
@@ -52,13 +56,22 @@ public class MainActivity extends AppCompatActivity {
 
         binding.getForecast.setOnClickListener(click -> {
             cityName = binding.editText.getText().toString();
-            String stringURL = "https://api.openweathermap.org/data/2.5/weather?q=" + URLEncoder.encode(cityName) + "&appid=7e943c97096a9784391a981c4d878b22&units=metric";
+            String stringURL = "https://api.pexels.com/v1/search?query=" + URLEncoder.encode(cityName);
 
             //this goes in the button click handler:
             JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringURL, null,
+
                     (response) -> {
 
-                        try {
+                try {
+                    JSONArray photosArray = response.getJSONArray("photos");
+                    for (int i = 0; i < photosArray.length(); i++) {
+                        JSONObject nextObj = photosArray.getJSONObject(i);
+                    }
+                }catch (JSONException ex){
+                    Log.e("Error:", ex.getMessage());
+                }
+                   /*     try {
                             JSONObject coord = response.getJSONObject( "coord" );
                            iconName = response.getJSONArray("weather").getJSONObject(0).getString("icon");
 
@@ -70,42 +83,61 @@ public class MainActivity extends AppCompatActivity {
                                image = BitmapFactory.decodeFile(pathname);
                            }
                            else {
-                               imgReq = new ImageRequest("https://openweathermap.org/img/w/" + iconName + ".png", new Response.Listener<Bitmap>() {
-                                   @Override
-                                   public void onResponse(Bitmap bitmap) {
+                               imgReq = new ImageRequest("https://openweathermap.org/img/w/" + iconName + ".png", ( bitmap ) ->{
                                        try {
                                            // Do something with loaded bitmap...
                                            image = bitmap;
                                            image.compress(Bitmap.CompressFormat.PNG, 100, MainActivity.this.openFileOutput(iconName + ".png", Activity.MODE_PRIVATE));
                                        }
                                        catch(Exception e){
-
+                                            Log.e("Error", e.getMessage());
                                        }
-                                   }
+
                                }, 1024, 1024, ImageView.ScaleType.CENTER, null, (error) -> {   });
                            }
 
 
                             String current, minTemp, maxTemp;
-                            current = minTemp = maxTemp = "";
+
+                           JSONObject main = response.getJSONObject("main");
+
+                            current = "The temperature is " + main.getDouble("temp");
+                            minTemp = "The min temperature is "+ main.getDouble("temp_min");
+                            maxTemp = "The max temperature is "+ main.getDouble("temp_max");
 
 
-                            binding.temp.setText("The current temperature is " + current);
-                            binding.temp.setVisibility(View.VISIBLE);
+                            runOnUiThread( (  )  -> {
 
-                            binding.minTemp.setText("The min temperature is " + minTemp);
-                            binding.minTemp.setVisibility(View.VISIBLE);
+                                binding.temp.setText(current);
+                                binding.temp.setVisibility(View.VISIBLE);
+
+                                binding.minTemp.setText( minTemp);
+                                binding.minTemp.setVisibility(View.VISIBLE);
+
+
+
+                            });
+
 
 
                             queue.add(imgReq);
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
-
+*/
                     },
-                    (error) -> {
-                Log.e("Error", "error");
-                    } );
+                    (error) -> {  Log.e("Error", "error"); }
+                    ) //closes the constructor ()
+                {
+                    //adding optional override function
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError
+                    {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        params.put("Authorization", "563492ad6f9170000100000147101c68beeb47838fe98505b3907dbb");
+                        return params;
+                }
+            };
             queue.add(request);
         });
     }
